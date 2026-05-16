896fd7c16b487c2d361629c17b3ad6cdf7c03d69
Irfan3006 <irfansyarifudin79@gmail.com>
2026-05-17 00:35:42 +0700

    add google analytics

--- assets/js/app.js
+++ assets/js/app.js
@@ -239,4 +239,17 @@ document.addEventListener('DOMContentLoaded', () => {
   
   document.body.insertAdjacentHTML('beforeend', renderLoader());
   updateNavbar();
+
+  // Google Analytics (gtag.js)
+  const gaId = 'G-Z5K95V81ZB';
+  const gtagScript = document.createElement('script');
+  gtagScript.async = true;
+  gtagScript.src = `https://www.googletagmanager.com/gtag/js?id=${gaId}`;
+  document.head.appendChild(gtagScript);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){ dataLayer.push(arguments); }
+  window.gtag = gtag;
+  gtag('js', new Date());
+  gtag('config', gaId);
 });
